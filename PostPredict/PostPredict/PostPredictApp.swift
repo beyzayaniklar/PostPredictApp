08f90f4041f035530d7fdc7bf6a08483c1728e9e
beyzayaniklar <beyzayaniklar@gmail.com>
2026-05-04 13:23:41 -0500

    Add files via upload

---
+++ PostPredict/PostPredict/PostPredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostPredictApp.swift
+//  PostPredict
+//
+//  Created by Yaniklar, Beyza on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct PostPredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
